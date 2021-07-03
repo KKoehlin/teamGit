946a7951995982ab3d730c93e3133295680ac047
Jackie Lai <jlai@bbbsci.org>
2021-07-03 11:53:49 -0400

    (Jackie) Installed Material UI and mapped through data to display all event names based on location

--- team-git-client/src/ticketmaster/TicketMasterApp.js
+++ team-git-client/src/ticketmaster/TicketMasterApp.js
@@ -6,8 +6,8 @@ const TicketMasterApp = (props) => {
     const [endDateTime, setEndDateTime] = useState("");
     const [results, setResults] = useState();
 
-    const currentDate = new Date();
-    const futureDate = new Date(currentDate);
+    // const currentDate = new Date();
+    // const futureDate = new Date(currentDate);
 
     // const futureDate = (dt, n) => {
     //     return new Date(dt.setMonth(dt.getMonth() + n));
@@ -18,8 +18,10 @@ const TicketMasterApp = (props) => {
 
     const fetcher = () => {
         const staticEndDate = "2021-08-03T00:00:00Z";
+        const staticRadius = "20";
+        const staticUnit = "miles"
         const apiKey = "tNGTJR4arQoc6d5dHyQ4HOamibbgtO8g";
-        fetch(`https://app.ticketmaster.com/discovery/v2/events.json?latlong=${props.lat},${props.long}&endDateTime=${staticEndDate}&apikey=${apiKey}`)
+        fetch(`https://app.ticketmaster.com/discovery/v2/events.json?latlong=${props.lat},${props.long}&radius=${staticRadius}&unit=${staticUnit}&endDateTime=${staticEndDate}&apikey=${apiKey}`)
             .then (res => {
                 if (res.status !== 200) {
                     throw new Error ('Fetch Error');
@@ -35,7 +37,8 @@ const TicketMasterApp = (props) => {
     return (
         <div>
             <button onClick={fetcher}>Find Events Nearby!</button>
-            {console.log(currentDate)}
+            <TMDisplay results={results}/>
+            {/* {console.log(currentDate)} */}
         </div>
     )
 }
