e31b63cfe3f9162d63bd8b8f658d2f51f5c81717
Jackie Lai <jlai@bbbsci.org>
2021-07-03 20:39:41 -0400

    (Jackie) Consistently displaying data from TM API as cards and began utilizing Material UI. Had to install another npm package for typography, so make sure to run npm i to update your scripts

--- team-git-client/src/ticketmaster/TicketMasterDisplay.js
+++ team-git-client/src/ticketmaster/TicketMasterDisplay.js
@@ -1,14 +1,53 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+    Box,
+    Card,
+    CardHeader,
+    CardActions,
+    CardContent,
+    CardMedia,
+    Button,
+    Typography
+} from '@material-ui/core';
+import '@fontsource/roboto';
 
 const TMDisplay = (props) => {
+    const useStyles = makeStyles({
+        root: {
+            maxWidth: 400,
+            marginTop: 12,
+        },
+        media: {
+            height: 0,
+            paddingTop: '56.25%',
+        },
+    });
+
+    const classes = useStyles();
+
     const eventMapper = () => {
-        return props.results._embedded.events.map((e) => {
-            return (
-                <p>{e.name}</p>
-            )
-        })
+        if (props.results) {
+            return props.results.map((e) => {
+                return (
+                    <Card className={classes.root}>
+                        <CardHeader
+                            title={e.name}
+                            subheader={e.dates.start.dateTime}
+                        />
+                        <CardMedia
+                            className={classes.media}
+                            image={e.images[6].url}
+                        />
+                        <CardActions>
+                            <Button size="medium" color="primary" variant="contained">Learn More</Button>
+                        </CardActions>
+                    </Card>
+                )
+            })
+        }
     }
-    
+
     return (
         <div>
             {eventMapper()}
