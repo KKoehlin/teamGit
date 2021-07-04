36b914e3f021362b15abbaa5937fa76eb2c8ad56
Jackie Lai <jlai@bbbsci.org>
2021-07-04 12:32:12 -0400

    (Jackie) Worked further with Material UI to stylize results from TM API into a grid

--- team-git-client/src/ticketmaster/TicketMasterDisplay.js
+++ team-git-client/src/ticketmaster/TicketMasterDisplay.js
@@ -8,41 +8,48 @@ import {
     CardContent,
     CardMedia,
     Button,
-    Typography
+    Typography,
+    Grid
 } from '@material-ui/core';
 import '@fontsource/roboto';
 
 const TMDisplay = (props) => {
     const useStyles = makeStyles({
         root: {
-            maxWidth: 400,
+            minWidth: 200,
             marginTop: 12,
         },
         media: {
             height: 0,
             paddingTop: '56.25%',
         },
+        gridContainer: {
+            paddingRight: '20px',
+            paddingLeft: '20px',
+        },
     });
 
     const classes = useStyles();
 
     const eventMapper = () => {
-        if (props.results) {
-            return props.results.map((e) => {
+        if (props.result) {
+            return props.result.map((e) => {
                 return (
-                    <Card className={classes.root}>
-                        <CardHeader
-                            title={e.name}
-                            subheader={e.dates.start.dateTime}
-                        />
-                        <CardMedia
-                            className={classes.media}
-                            image={e.images[6].url}
-                        />
-                        <CardActions>
-                            <Button size="medium" color="primary" variant="contained">Learn More</Button>
-                        </CardActions>
-                    </Card>
+                    <Grid item xs={12} sm={6} md={4}>
+                        <Card className={classes.root}>
+                            <CardHeader
+                                title={e.name}
+                                subheader={e.dates.start.dateTime}
+                            />
+                            <CardMedia
+                                className={classes.media}
+                                image={e.images[6].url}
+                            />
+                            <CardActions style={{ justifyContent: 'center' }}>
+                                <Button href={e.url} target="_blank" size="small" color="secondary" variant="contained">Buy Tickets</Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
                 )
             })
         }
@@ -50,7 +57,9 @@ const TMDisplay = (props) => {
 
     return (
         <div>
-            {eventMapper()}
+            <Grid container className={classes.gridContainer} spacing={4} justify="center">
+                {eventMapper()}
+            </Grid>
         </div>
     )
 }
