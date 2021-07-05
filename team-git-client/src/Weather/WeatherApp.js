50c66f9feaf016ce4ba36040e449899acd196ce7
Brad Snyder <bsnyder13@gmail.com>
2021-07-05 09:41:21 -0400

    pushing to brad branch" git push --set-upstream origin brad
    git add .

---
+++ team-git-client/src/Weather/WeatherApp.js
@@ -0,0 +1,29 @@
+//import React, { useState, useEffect } from 'react';
+//import WeatherDisplay from './WeatherDisplay';
+import GetLocation from '..location/GetLocation';
+
+//let apiKey = "2d765e4fdc07f7fef0fa8fda05c45967";
+
+//let lat = "39.791000"
+//let lon = "-86.148003"
+//let url='api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}'
+/*
+const WeatherApp = (props) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+*/
+const fetcher = () => {
+    fetch('http://api.openweathermap.org/data/2.5/weather?lat=${props.lat}&lon=${props.long}&appid=2d765e4fdc07f7fef0fa8fda05c45967')
+        .then(res => res.json())
+        .then(json => {
+            console.log(json)
+
+            let tempF = url + 'units=imperial';
+            console.log(tempF)
+        })
+
+    export default WeatherApp;
