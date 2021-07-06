b261d9fa503d7ac6923dc9aa45af153a70e7fee8
Brad Snyder <bsnyder13@gmail.com>
2021-07-05 21:55:18 -0400

    updated display and button

--- team-git-client/src/Weather/WeatherApp.js
+++ team-git-client/src/Weather/WeatherApp.js
@@ -2,44 +2,46 @@ import React, { useState, useEffect } from 'react';
 import WeatherDisplay from './WeatherDisplay';
 //import GetLocation from '..location/GetLocation';
 
+import { Button } from '@material-ui/core';
 
 
 //let lat = "39.791000"
 //let lon = "-86.148003"
 //let url='api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=2d765e4fdc07f7fef0fa8fda05c45967'
 
-
-
-let url = 'http://api.openweathermap.org/data/2.5/weather?lat=39.791000&lon=-86.148003&appid=';
+//let url = 'http://api.openweathermap.org/data/2.5/weather?lat=39.791000&lon=-86.148003&appid=';
 
 const WeatherApp = (props) => {
 
-    const [weather, setWeather] = useState();
+    const [weatherInfo, setWeatherInfo] = useState();
+    const [unit, setUnit] = useState("metric");
 
     let apiKey = "dcee62d41438be11823b7568498cc8e9";
 
-    const fetcher = () => {
-        fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${props.lat}&lon=${props.long}&appid=${apiKey}`)
-            .then(res => res.json())
-            .then(json => {
-                console.log(json)
-                setWeather(json)
+    const toggleBtn = () => {
+        return (
+            (unit === "metric") ? setUnit("imperial") : setUnit("metric")
+        )
+    }
 
-                //let tempF = url + apiKey + '&units=imperial';
-                //console.log(tempF)
+const fetcher = () => {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${props.lat}&lon=${props.long}&appid=${apiKey}&units=${unit}`)
+        .then(res => res.json())
+        .then(json => {
+            setWeatherInfo(json)
+            console.log(json)
 
-                //let tempC = url + apiKey + '&units=metric';
-                //console.log(tempC)
-            })
+        })
+        .catch(err => console.log(err))
     }
 
     return (
         <div>
-            <WeatherDisplay weather={weather} />
+            <Button size="medium" variant="contained" onClick={fetcher}>What's the weather?</Button>
+            <br />
+            <Button size="small" variant="contained" onClick={toggleBtn}>Change weather Unit</Button>
+            <WeatherDisplay weatherInfon={weatherInfo} />
         </div>
     )
 }
-
-
-
-export default WeatherApp;
+    export default WeatherApp;
--- team-git-client/src/Weather/WeatherDisplay.js
+++ team-git-client/src/Weather/WeatherDisplay.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+    Box,
+    Card,
+    CardHeader,
+    CardActions,
+    CardContent,
+    CardMedia,
+    Button,
+    Typography,
+    Grid
+} from '@material-ui/core';
 
 const WeatherDisplay = (props) => {
+
+    const display = () => {
+        if (props.weatherInfo) {
+            return (
+                <Card>
+                    <CardHeader title={props.weatherInfo.name} />
+                    <CardContent>
+                        <Typography>
+                            {props.weatherInfo.weather[0].description}
+                        </Typography>
+                    </CardContent>
+                </Card>
+            )
+        }
+    }
+
     return(
         <div>
-            <h1>{props.name}</h1>
-            {console.log(props.name)}
+            {display()}
         </div>
     )
 }
